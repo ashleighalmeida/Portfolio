18bac369284715d6e0e8ba7177b503329d175e48
Ashleigh <ashleighalmeida@gmail.com>
2015-06-29 18:34:11 -0400

    toggle sidebar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,12 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+$(function() {
+  $(".nav").click(function() {
+    $(".sidebar").animate({width: 'toggle'});
+  })  
+})
+
+
